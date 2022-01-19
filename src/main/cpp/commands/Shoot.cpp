5774b087fa8500a744cc5017f65a076f282d2d5a
Eytan Zury <eytanzury@gmail.com>
2022-01-19 19:32:42 +0200

    New Shoot Command (for RendallPID)

---
+++ src/main/cpp/commands/Shoot.cpp
@@ -0,0 +1,38 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+#include "commands/Shoot.h"
+
+Shoot::Shoot(RendallPID* rendallpid) : m_rendallpid{rendallpid} {
+  // Use addRequirements() here to declare subsystem dependencies.
+  AddRequirements({rendallpid});
+}
+
+// Called when the command is initially scheduled.
+void Shoot::Initialize() {
+  this->m_rendallpid->Disable();
+  this->m_rendallpid->load(false);
+  this->m_rendallpid->SetSetpoint(powers::kShootPower);
+}
+
+// Called repeatedly when this Command is scheduled to run
+void Shoot::Execute() {
+  if (m_rendallpid->GetController().AtSetpoint())
+    m_rendallpid->load(true, powers::kLoadPower);
+  else
+    m_rendallpid->load(false);
+
+  m_rendallpid->Enable();
+}
+
+// Called once the command ends or is interrupted.
+void Shoot::End(bool interrupted) {
+  this->m_rendallpid->Disable();
+  m_rendallpid->load(false);
+}
+
+// Returns true when the command should end.
+bool Shoot::IsFinished() {
+  return false;
+}
